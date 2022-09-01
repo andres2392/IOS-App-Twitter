b81496d65d3a88eb8a62845e8dca93e6893003d5
Andres <andres.gonzalez2392@gmail.com>
2022-09-01 15:38:37 -0400

    Initial Commit

---
+++ TwitterC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterC
+//
+//  Created by PDWS on 9/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterC/TwitterCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCApp.swift
+//  TwitterC
+//
+//  Created by PDWS on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
